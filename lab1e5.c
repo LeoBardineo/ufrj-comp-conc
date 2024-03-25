30ad228afaaf49f982c29b82db9c9c9e3197366c
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2024-03-25 00:36:47 -0300

    final lab1e5

--- lab1e5.c
+++ lab1e5.c
@@ -3,6 +3,7 @@
 #include <pthread.h>
 
 int* array;
+int* arrayTeste;
 typedef struct {
     int initialIndex;
     int lastIndex;
@@ -25,7 +26,7 @@ void printaArray(int N) {
 }
 
 int main(int argc, char* argv[]) {
-    puts("Programa concorrente para elevar ao quadrado de um array");
+    puts("Programa concorrente para elevar ao quadrado todos os elementos de um array");
     if(argc < 3) { 
         printf("ERRO 400: informe a quantidade de threads e tamanho do array <%s> <nthreads> <tamanho>\n", argv[0]);
         exit(-1);
@@ -46,31 +47,44 @@ int main(int argc, char* argv[]) {
       exit(-2);
     }
 
+    arrayTeste = malloc(N * sizeof(int));
+    if (arrayTeste == NULL) {
+      printf("ERRO 500: erro ao alocar memória para o arrayTeste\n");
+      exit(-2);
+    }
+
     for(int i = 0; i < N; i++) {
         printf("Entre com o %dº número: ", i+1);
         scanf("%d", &(array[i]));
+        arrayTeste[i] = array[i];
     }
     puts("\n");
     printf("Array inicial:\n");
     printaArray(N);
 
-    if(N % M == 0) {
-        int passo = N/M;
-        for(int i = 0; i < N; i += passo) {
-            t_argsArray *argsArray = malloc(sizeof(t_argsArray));
-            if (argsArray == NULL) {
-                printf("ERRO 500: erro ao alocar memória para o argsArray\n");
-                exit(-2);
-            }
-
-            argsArray->initialIndex = i;
-            argsArray->lastIndex = i + passo - 1;
-            if (pthread_create(&tid_sistema[mThreadsCriadas], NULL, calculaQuadrado, (void*) argsArray)) {
-                printf("ERRO 500: erro ao criar a thread\n");
-                exit(-2);
-            }
-            mThreadsCriadas++;
+    int elementosExtras = N % M;
+    int passo = N / M;
+    puts("");
+    for(int i = 0; i < N; i += passo) {
+        t_argsArray *argsArray = malloc(sizeof(t_argsArray));
+        if (argsArray == NULL) {
+            printf("ERRO 500: erro ao alocar memória para o argsArray\n");
+            exit(-2);
         }
+
+        argsArray->initialIndex = i;
+        argsArray->lastIndex = i + passo + elementosExtras - 1;
+        if (pthread_create(&tid_sistema[mThreadsCriadas], NULL, calculaQuadrado, (void*) argsArray)) {
+            printf("ERRO 500: erro ao criar a thread\n");
+            exit(-2);
+        }
+
+        if(elementosExtras > 0){
+            i += elementosExtras;
+            elementosExtras -= elementosExtras;
+        }
+        
+        mThreadsCriadas++;
     }
 
     for (int i = 0; i < mThreadsCriadas; i++) {
@@ -80,9 +94,27 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    puts("\n");
+    puts("");
     printf("Array final:\n");
     printaArray(N);
+    
+    puts("\n");
+    printf("Teste:\n");
+    int testePassou = 1;
+    for(int i = 0; i < N; i++){
+        printf("%d * %d == %d \t| ", arrayTeste[i], arrayTeste[i], array[i]);
+        if(arrayTeste[i] * arrayTeste[i] == array[i]) {
+            printf("CORRETO\n");
+        } else {
+            printf("FALSO\n");
+            testePassou = 0;
+        }
+    }
+    if(testePassou) {
+        printf("O teste passou");
+    } else {
+        printf("O teste falhou");
+    }
 
     pthread_exit(NULL);
 }
